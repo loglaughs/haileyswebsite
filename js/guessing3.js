e0242539adebf0b887a1fa7f983b59bb0cd80cae
loglaughs <32936054+loglaughs@users.noreply.github.com>
2017-12-08 08:16:37 -0600

    Create guessing3.js

---
+++ js/guessing3.js
@@ -0,0 +1,22 @@
+document.addEventListener("keypress", check);
+function check() {
+  var x = event.keyCode;
+  if (x == 13) {
+    return guess();
+  }
+  else {
+    return false;
+  }
+}
+function guess() {
+  var guess = document.getElementById("guess").value;
+  if (guess !== "energy") {
+    document.getElementById("out").innerHTML = "That's not correct!";
+    document.getElementById("guess").value = "";
+  }
+  else {
+    document.getElementById("out").innerHTML = "That's Correct!";
+    document.getElementById("guess").value = "energy";
+    document.getElementById("next").style.display = "block";
+  }
+}
